dbdf79b09fb0937299ddde0921701a57711cf11e
Dan <danikk066@gmail.com>
2023-03-15 23:15:40 +0300

    Login and logout

--- pybase/main/forms.py
+++ pybase/main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import *
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -44,3 +44,10 @@ class RegisterUserForm(UserCreationForm):
             'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
         }
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(
+        label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- pybase/main/views.py
+++ pybase/main/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout, login
 
 from .models import *
 from .forms import *
@@ -75,8 +77,8 @@ def contact(request):
     return HttpResponse('Обратная связь')
 
 
-def login(request):
-    return HttpResponse('Авторизация')
+# def login(request):
+#     return HttpResponse('Авторизация')
 
 
 def pageNotFound(request, exception):
@@ -144,3 +146,26 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'main/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
